90813295b4493068762fc3734d1d9c4f5ee8a7c4
xatia lekishvili <khatialekishvili@xatias-MacBook-Pro.local>
2022-12-04 23:05:29 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationsadadsadsada/fragments/ThirdFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplicationsadadsadsada.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplicationsadadsadsada.R
+
+class ThirdFragment : Fragment(R.layout.fragment_third){
+}
